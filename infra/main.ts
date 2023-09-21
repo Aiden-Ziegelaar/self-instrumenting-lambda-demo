b18ef26a7f70144da061564013ce675a0cd2c9d2
Aiden Ziegelaar <Aiden-Ziegelaar@users.noreply.github.com>
2023-09-21 11:28:27 +0800

    feat: Added presentation aspects

--- infra/main.ts
+++ infra/main.ts
@@ -5,20 +5,66 @@ import * as crypto from "crypto";
 import * as fs from "fs";
 
 import { IConstruct, Construct } from "constructs";
-import { IAspect, Aspects, App, TerraformStack } from "cdktf";
+import { IAspect, Aspects, App, TerraformStack, Annotations } from "cdktf";
 import * as aws from "@cdktf/provider-aws";
 
 const PWD = process.cwd();
 
 const APP_NAME = "self-instrumenting-lambda-demo";
 
+function checkWafRulesValid(input: aws.wafWebAcl.WafWebAclRulesList) {
+  return !!input; // we might check for inclusion of a specific rule group here without excludes
+}
+
+export class CloudfrontWafAspect implements IAspect {
+  constructor(private wafArn: string) {}
+
+  visit(node: IConstruct) {
+    if (node instanceof aws.cloudfrontDistribution.CloudfrontDistribution) {
+      if (!node.webAclId) {
+        Annotations.of(node).addInfo("Added default WAF to Cloudfront")
+        node.webAclId = this.wafArn;
+      }
+    }
+  }
+}
+
+export class WafMinimumStandardsAspect implements IAspect {
+  visit(node: IConstruct) {
+    if (node instanceof aws.wafWebAcl.WafWebAcl) {
+      if (checkWafRulesValid(node.rules)) {
+        Annotations.of(node).addError(
+          'WAF does not meet minimum standards,' +
+          'see companydocs.com/waf for more info'
+        );
+      }
+    }
+  }
+}
+
+export class ValidateLambdaIsPrefixed implements IAspect {
+  constructor(private prefix: string) {}
+
+  visit(node: IConstruct) {
+    if (node instanceof aws.lambdaFunction.LambdaFunction) {
+      if (
+        node.functionName &&
+        !node.functionNameInput?.startsWith(this.prefix)
+      ) {
+        Annotations.of(node).addError(
+          `Each Lambda name needs to start with ${this.prefix}`
+        );
+      }
+    }
+  }
+}
+
 export class LambdaLayerInstrumentAspect implements IAspect {
   constructor(private instrumentationLayerArn: string) {}
 
   // This method is called on every Construct within the specified scope (resources, data sources, etc.).
-  visit(visited_node: IConstruct) {
-    if (visited_node instanceof aws.lambdaFunction.LambdaFunction) {
-      const node = visited_node as aws.lambdaFunction.LambdaFunction;
+  visit(node: IConstruct) {
+    if (node instanceof aws.lambdaFunction.LambdaFunction) {
       // We need to take the input value to not create a circular reference
       const currentEnv = node.environmentInput ?? {};
       const currentHandler = node.handlerInput ?? "index.hander";
